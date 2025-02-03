71daf3367ed5d8c1c05cda6fd76e29387484b360
Assan Hedqvist <secka@kth.se>
2025-02-03 14:04:56 +0100

    fixing deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,35 @@ import { db } from "~/server/db";
 
 const mockUrl = [
   "https://l0237siktb.ufs.sh/f/G7g2e028EcPgWYItYPF2hYxos57tZAGQazgdDjLiMnNVFOJU",
-]
+];
 
 const mockImage = mockUrl.map((url, index) => ({
   id: index + 1,
   url,
 }));
 
-export default async function HomePage() {
-
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
-
+export default function HomePage({ posts }: { posts: { id: number; name: string }[] }) {
   return (
-    <main className="">
-      <div className ="flex flex-wrap gap-4">
+    <main>
+      <div className="flex flex-wrap gap-4">
         {posts.map((post) => (
-          <div key ={post.id}>{post.name}</div>
-
+          <div key={post.id}>{post.name}</div>
         ))}
         {mockImage.map((image, index) => (
           <div key={image.id + "-" + index} className="w-48">
-            <img src={image.url} alt="image"/>
-      </div>
+            <img src={image.url} alt="image" />
+          </div>
         ))}
       </div>
-      Hello (deployed in vercel)
+      Hello (deployed in Vercel)
     </main>
   );
+}
+
+export async function getServerSideProps() {
+  const posts = await db.query.posts.findMany();
+
+  return {
+    props: { posts },
+  };
 }
