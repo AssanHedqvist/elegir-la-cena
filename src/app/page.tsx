bc31d31bc21e0d5b539f3dbd26a40d2ccd13adb3
Assan Hedqvist <secka@kth.se>
2025-02-03 14:06:53 +0100

    fixing error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,32 @@ import { db } from "~/server/db";
 
 const mockUrl = [
   "https://l0237siktb.ufs.sh/f/G7g2e028EcPgWYItYPF2hYxos57tZAGQazgdDjLiMnNVFOJU",
-];
+]
 
 const mockImage = mockUrl.map((url, index) => ({
   id: index + 1,
   url,
 }));
 
-export default function HomePage({ posts }: { posts: { id: number; name: string }[] }) {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
-    <main>
-      <div className="flex flex-wrap gap-4">
+    <main className="">
+      <div className ="flex flex-wrap gap-4">
         {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
+          <div key ={post.id}>{post.name}</div>
+
         ))}
         {mockImage.map((image, index) => (
           <div key={image.id + "-" + index} className="w-48">
-            <img src={image.url} alt="image" />
-          </div>
+            <img src={image.url} alt="image"/>
+      </div>
         ))}
       </div>
-      Hello (deployed in Vercel)
+      Hello (deployed in vercel)
     </main>
   );
-}
-
-export async function getServerSideProps() {
-  const posts = await db.query.posts.findMany();
-
-  return {
-    props: { posts },
-  };
 }
