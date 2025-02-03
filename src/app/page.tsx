de743bb2652bfe386996f8541e08ca8aeb2a469b
Assan Hedqvist <secka@kth.se>
2025-02-03 13:38:03 +0100

    added db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrl = [
   "https://l0237siktb.ufs.sh/f/G7g2e028EcPgWYItYPF2hYxos57tZAGQazgdDjLiMnNVFOJU",
@@ -9,12 +10,20 @@ const mockImage = mockUrl.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <main className="">
       <div className ="flex flex-wrap gap-4">
-        {mockImage.map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key ={post.id}>{post.name}</div>
+
+        ))}
+        {mockImage.map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} alt="image"/>
       </div>
         ))}
